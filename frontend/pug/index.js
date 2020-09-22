7dc6d2af8f138c2f30e2c6430cfb0cdfc4c8d4b6
SerSerch <serserch@yandex.ru>
2020-09-22 22:24:52 +0300

    add animation

--- frontend/pug/index.js
+++ frontend/pug/index.js
@@ -1,6 +1,31 @@
+import AOS from 'aos';
+
 import 'normalize.css';
+import 'aos/dist/aos.css';
 import './swiper-bundle.min.css';
 import './sass/index.scss';
+
+AOS.init({
+  // Global settings:
+  disable: 'phone', // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+  startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+  initClassName: 'aos-init', // class applied after initialization
+  animatedClassName: 'aos-animate', // class applied on animation
+  useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+  disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+  debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+  throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+
+
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 50, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 800, // values from 0 to 3000, with step 50ms
+  easing: 'linear', // default easing for AOS animations
+  once: true, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: 'bottom-bottom', // defines which position of the element regarding to window should trigger the animation
+});
 
 const Swiper = require('./swiper-bundle.min.js');
 
@@ -119,12 +144,4 @@ const sendForm = function (e) {
 
 for (let i of document.querySelectorAll('form')) {
   i.addEventListener('submit', sendForm);
-}
-
-function checkPersonal(e) {
-  e.currentTarget.parentNode.parentNode.querySelector('[type=submit]').disabled = !e.target.checked;
-}
-
-for (let i of document.querySelectorAll('input[name=check-personal]')) {
-  i.addEventListener('change', checkPersonal);
 }
